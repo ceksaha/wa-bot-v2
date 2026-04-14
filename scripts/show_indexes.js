6fff58dfd3d2baf3b320f09fe1371117a71022fb
cikeas <xmeris.k510@gmail.com>
2026-04-14 12:29:53 +0700

    Added show indexes script

---
+++ scripts/show_indexes.js
@@ -0,0 +1,15 @@
+const { sequelize } = require('./src/config/database');
+
+async function check() {
+    try {
+        await sequelize.authenticate();
+        const [results] = await sequelize.query('SHOW INDEX FROM Tenants');
+        console.log(JSON.stringify(results, null, 2));
+        process.exit(0);
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1);
+    }
+}
+
+check();
